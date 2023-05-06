7606664d9a56947d923c749232ce27c0fa40c813
ramses237 <ramsesndame34@gmail.com>
2023-05-06 23:15:20 +0100

    feat:creation of the home page, finish login fonction and display message
    i create and custom the home page after login success and also performe the login fonction
    
    Review_by: Victor
    Refs:#14

--- frontend-security-test/src/views/Home.vue
+++ frontend-security-test/src/views/Home.vue
@@ -1,7 +1,47 @@
 <template>
-  <HelloWorld />
+  <main class="main-home">
+    <div>
+      <img src="https://img.newstank.fr/image/b7c5e21c5e5e2c6158f016811c6c992b/465/218/cooptalis-devient-anywr.jpg"
+        alt="anywr">
+      <div>
+        <p>
+          eader du recrutement international et de la mobilité professionnelle
+        </p>
+      </div>
+      <v-chip
+      class="ma-2"
+      color="primary"
+      variant="elevated"
+      @click="gotToProfil"
+    >
+      Profil
+      <v-icon end icon="mdi-account-outline"></v-icon>
+    </v-chip>
+    </div>
+
+  </main>
 </template>
 
-<script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import router from '@/router';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  methods:{
+    gotToProfil:{
+      router.push('/profil')
+    }
+  }
+})
+
 </script>
+
+<style lang="scss">
+.main-home {
+  background: #503191;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
